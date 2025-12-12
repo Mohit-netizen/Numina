fc56ee1934722beb626e6686d722b6cd24af6d18
Mohit Kumar <160260351+Mohit-netizen@users.noreply.github.com>
2025-12-13 02:20:09 +0530

    Initial commit

---
+++ Numina.R
@@ -0,0 +1,148 @@
+#11-12-2025
+
+##Data Visualization
+
+head(Gasoline_Retail_Prices_Weekly_Average_by_Region_Beginning_January_2017)
+
+plot(Gasoline_Retail_Prices_Weekly_Average_by_Region_Beginning_January_2017,type="l", main="Time-series",col="red" , lwd=2)
+
+getwd()
+a=3
+b=5
+a+b
+
+
+library(car)
+data=read.csv(s)
+Salary_Data <- read_csv("D:/Artificial intelligence and concepts of R programming/R studio/Salary_Data.csv")
+
+getwd()
+setwd("c:/Users/Mohit/Documents/GitHub/Numina/")
+getwd()
+Data= read.csv("D:/AI & CRP/R S/Salary_Data.csv")
+Data= read.csv("D:/AI & CRP/R S/Gasoline_Retail_Prices_Weekly_Average_by_Region__Beginning_January_2017.csv")
+gas_prices <- read.csv("D:/AI & CRP/R S/Gasoline_Retail_Prices_Weekly_Average_by_Region__Beginning_January_2017.csv")
+
+View(gas_prices)
+View(Gasoline_Retail_Prices_Weekly_Average_by_Region__Beginning_January_2017)
+dim(Data)
+
+
+
+head(Data)
+head(Data,10)
+str(Data)
+colnames(Data)
+install.packages("skimr")   # if not already installed
+library(skimr)
+
+brief(Data)
+library(skimr)
+brief(Data)
+skim(Data)
+library(skimr)
+skim(Data)
+colnames(Data)[1]="Name"
+colnames(Data)[1]
+colnames(Data)[1]
+colnames(Data)[1]="Subject"
+colnames(Data)[1]
+head(Data,10)
+colnames(Data)[1]="Years Of Experience"
+head(Data,10)
+colnames(Data)[2:4]=c ("Title","ID","Agencyname")
+set
+getwd()
+setwd("D:/AI & CRP/Numina/")
+Data= read.csv("D:/AI & CRP/Numina/all data.RData")
+getwd()
+skim(Data)
+library(readr)
+Salary_Data <- read_csv("Salary_Data.csv")
+View(Salary_Data)
+Salary_Data[c("Title", "ID", "Agencyname")]
+str(Salary_Data)
+colnames(Salary_Data)[3:5] <- c("Title", "ID", "Agencyname")
+ncol(Salary_Data)
+colnames(Salary_Data)[3:5]
+colnames(Salary_Data)[3:4] <- c("Title", "ID")
+Salary_Data[c("Title", "ID", "Agencyname")] <- ""
+Salary_Data[1,3]="Jai"
+
+
+brief(Data)
+skim(Data)
+spec()
+head(Data,10)
+colnames(Salary_Data)[1]="Years Of Experience"
+head(Data,10)
+data=read.csv("Salary_Data.csv")
+
+colnames(Data)[2]
+colnames(Data)[2:4]=c ("Title","ID","Agencyname")
+head(Data,10)
+colnames(Data)
+str(Data)
+Data[c("Title", "ID", "Agencyname")] <- ""
+str(Salary_Data)
+str(Salary_Data)
+Salary_Data[c("Title", "ID", "Agencyname")] <- ""
+View(Salary_Data)
+
+x=c(0, NA, 2, 3, 4, -0.5, 0.2)
+x
+levels(factor(x))
+is.na(NA)
+! is.na(NA)
+x>2 & is.na()
+x>2 & !is.na(x)
+(x==0 | x==2) & !is.na(x)
+(x==0 | x==2) & is.na(x)
+(x==0 | x==2)
+head(data)
+Data= read.csv("D:/AI & CRP/R S/Salary_Data.csv")
+colnames(Data)[2]
+View(Data)
+data_1=Data
+View(Data)
+View(data_1)
+rm(Data1)
+nrow(data_1)
+ncol(data_1)
+str(data_1)
+brief(data_1)
+spec(data_1)
+skim(data_1)
+col(data_1)
+dim(data_1)
+system("git remote -v")
+data_1[1,3]="Jai"
+View(Salary_Data)
+Salary_Data[c("Title", "ID", "Agencyname")]
+Salary_Data[2,3]="Sam"
+Salary_Data[2,3]="Sam"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
